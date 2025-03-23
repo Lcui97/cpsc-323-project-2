e8201eb93fbafd16845a0e4a268c3c71d8650066
kalmk <111930167+kalmk@users.noreply.github.com>
2025-03-23 12:21:43 -0700

    work in progress
    I want to read each char from the input and if there is either a terminal or a non-terminal, we use the table to decide to either shift(append) or reduce (replace).

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from get_input import *
+from table import *
+
 def main():
-    return
+    input = get_input("./test_cases/10.in")
+
+    stack = []
+    word = ""
+    while True:
+        for char in input:
+            word += char
+            if word in table.terminals:
+                stack.append(word)
+                word = ""
+
+                # now check the grammar
+
+            else:
+                continue
+
+        
 
 if __name__ == "__main__":
     main()
