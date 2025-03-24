1f2ebf3079550e6a713ce84fb1efe0ee5f021969
kalmk <111930167+kalmk@users.noreply.github.com>
2025-03-23 18:08:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from get_tokens import *
 import table
 
 def main():
-    input = get_tokens("./test_cases/7.in")
+    input = get_tokens("./test_cases/1.in")
     input.append("$")
     print(input)
 
     stack = [0]
+    index = 0
+    
+    while True:
+        current_state = stack[-1]
+        current_token = input[index]
+
+        action = table.table[current_state][table.valid_tokens.index(current_token)]
+        print(action)
+        break
+        
     
         
 
